a192516e145728ac9666113e99d6b12dd856836c
IanL73 <ianl73@nycstudents.net>
2025-11-13 14:00:01 -0500

    it's now time for our feature presentation. feacher coming straight from your house coming straight from you house coming he's the one coming the king of only he's groovy and never glooby you can't get this from an egg the sensation of your screen the show that makes you scream say it with him folks Mr. (Ant) Tenna's TV Time!

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,15 @@
 import './style.css'
 import { setupCounter } from './counter.js'
 
+
+// Doing digital art gallery
+//create array of entries
+//function to insert filtered entries
+//program entries to show popup when clicked
+//make input fields for new image and accompanying description
+//highlighted entry of the day that changes on the minute for testing purposes
+//light mode dark mode
+
 document.querySelector('#app').innerHTML = `
   <button class="button">click this button to do... something i guess</button>
 `
