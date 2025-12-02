25f4fc6a90693f9612377c75060567d23a6688f1
IanL73 <ianl73@nycstudents.net>
2025-12-02 14:01:31 -0500

    aaaaaaaaaaaaa

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import { setupCounter } from './counter.js'
-import charlie from './array.json'
+import gallery from './array.json'
 
 
 document.querySelector(".btn").addEventListener("click", function () {
@@ -15,26 +15,30 @@ document.querySelector(".btn").addEventListener("click", function () {
 });
 
 
-function inject(product){
+function inject(piece){
     //query the html where we inject the card
     const container = document.querySelector(".main");
     container.insertAdjacentHTML(
         "beforeend",
-        `<div class="item" data-title="${product.title}" price=${product.price}>
-            <h4 class="item-title">${product.title}</h4>
-            <img class="item-image" src="${product.image}"/>
+        `<div class="item" data-title="${piece.title}" arbitrary-number=${piece.price}>
+            <h4 class="item-title">${piece.title}</h4>
+            <img class="item-image" src="${piece.image}"/>
         </div>`
     );
 }
-charlie.forEach((product) => inject(product))
+gallery.forEach((piece) => inject(piece))
 
 document.querySelectorAll(".item").forEach((item)=>item.addEventListener("click", function () {
   document.querySelector(".blurb-container").innerHTML = ""
+  console.log(gallery)
+  let title = event.target.closest(".item").getAttribute("data-title")
+  let selecteditem = gallery.find((item)=>item.title === title)
   document.querySelector(".blurb-container").insertAdjacentHTML(
     "afterbegin",
     `
-    <h4 class="blurb-author">Me. I made this</h4>
-    <h4 class="blurb-text">description words description words description words description words description words description words </h4>
+    <h4 class="blurb-author">Artist: ${selecteditem.artist}</h4>
+    <h4 class="blurb-text">${selecteditem.desc}</h4>
+    <img class="blurb-image" src="${selecteditem.image}"/>
     `
   )
   document.querySelector(".blurb").showModal();
